a220a4599feadafb12976b2801e3fb6e61aa4e91
Thilo Fischer <thilo.leon.fischer@tuhh.de>
2017-01-06 13:31:00 +0100

    Added powerline theme

---
+++ home/workspace/powerline-shell/themes/darcula.py
@@ -0,0 +1,35 @@
+class Color(DefaultColor):
+    USERNAME_FG = 15
+    USERNAME_BG = 4
+    USERNAME_ROOT_BG = 1
+
+    HOSTNAME_FG = 15
+    HOSTNAME_BG = 10
+
+    HOME_SPECIAL_DISPLAY = False
+    PATH_FG = 6
+    PATH_BG = 7
+    CWD_FG = 6
+    SEPARATOR_FG = 11
+
+    READONLY_BG = 1
+    READONLY_FG = 7
+
+    REPO_CLEAN_FG = 14
+    REPO_CLEAN_BG = 0
+    REPO_DIRTY_FG = 3
+    REPO_DIRTY_BG = 0
+
+    JOBS_FG = 4
+    JOBS_BG = 8
+
+    CMD_PASSED_FG = 3
+    CMD_PASSED_BG = 15
+    CMD_FAILED_FG = 15
+    CMD_FAILED_BG = 1
+
+    SVN_CHANGES_FG = REPO_DIRTY_FG
+    SVN_CHANGES_BG = REPO_DIRTY_BG
+
+    VIRTUAL_ENV_BG = 15
+    VIRTUAL_ENV_FG = 2
