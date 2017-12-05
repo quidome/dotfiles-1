4d9fc597552b604e10ba80062f0926c001df5e9a
Thilo Fischer <thilo.leon.fischer@tuhh.de>
2017-12-05 22:13:02 +0100

    Fix urxvt background color and other stuff

--- home/.ncmpcpp/notify.sh
+++ home/.ncmpcpp/notify.sh
@@ -3,4 +3,4 @@
 artist=$(mpc -f %artist% | head -n1)
 song=$(mpc -f %title% | head -n1)
 album=$(mpc -f %album% | head -n1)
-notify-send "$song" "$artist - $album"
+notify-send "<b>$song</b>" "$artist - $album"
